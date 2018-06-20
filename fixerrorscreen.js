aa14842cb494071ac42b25d391ecfdc859b27d81
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2018-06-20 12:00:19 -0300

    Create fixerrorscreen.js

---
+++ fixerrorscreen.js
@@ -0,0 +1,10 @@
+$('#popupDiv_ara').css('z-index',999999999);
+$('#popupDiv_ara').css('margin-top',-100);
+$('#popupDiv_ara').css('margin-left',-10);
+$('#popupDiv_ara').children().css('overflow','hidden');
+$('#popupDiv_ara').children().children().children('tbody').css('display','block');
+$('#popupDiv_ara').children().children().children('tbody').css('width',500);
+$('#popupDiv_ara').children().children().children('tbody').css('height',500);
+$('#popupDiv_ara').children().children().children('tbody').css('overflow','scroll');
+$('#popupDiv_ara').children().children().children('tbody').css('overflow-y','scroll');
+$('#popupDiv_ara').children().children().children('tbody').css('overflow-x','scroll');
